5113d53f5d36540045d03a42818147a2681cee3d
João Marcos Pereira De Souza <112454993+joaomarcospsza@users.noreply.github.com>
2022-08-30 15:46:02 -0300

    CHECK
    criando a nova regra CHECK para ativo e adicionando novos valores

--- Curso.sql
+++ Curso.sql
@@ -7,12 +7,15 @@ CREATE DATABASE aula_banco;
 USE aula_banco;
 
 CREATE TABLE estado(
-id INT PRIMARY KEY,
-nome VARCHAR(200),
-sigla CHAR(2),
-ativo CHAR(1),
-data_cadastro DATETIME
+id INT NOT NULL PRIMARY KEY,
+nome VARCHAR(200) NOT NULL UNIQUE,
+sigla CHAR(2) NOT NULL UNIQUE,
+ativo CHAR(1) NOT NULL,
+data_cadastro DATETIME,
+CHECK (ativo IN('S', 'N'))
 );
 
 INSERT INTO estado(id, nome, sigla, ativo, data_cadastro) VALUE('1', 'PARANÁ', 'PR', 'S', '2022-08-30');
+INSERT INTO estado(id, nome, sigla, ativo, data_cadastro) VALUE('2', 'GOIAS', 'GO', 'S', '2022-05-30');
+INSERT INTO estado(id, nome, sigla, ativo, data_cadastro) VALUE('3', 'MATO GROSSO', 'MT', 'S', '2022-04-30');
 SELECT id, nome, sigla, ativo, data_cadastro FROM estado;
