db72c7a20aedbe76cba8c3c677e8fe1de0ba0938
João Marcos Pereira De Souza <112454993+joaomarcospsza@users.noreply.github.com>
2022-08-31 01:33:00 -0300

    Padronização
    Verificando se necessita de padronização

--- Curso.sql
+++ Curso.sql
@@ -1,7 +1,4 @@
-/** 
-eliminando base de dados
-só executa se existir
-*/
+
 DROP DATABASE IF EXISTS aula_banco;
 CREATE DATABASE aula_banco;
 USE aula_banco;
@@ -17,13 +14,13 @@ data_cadastro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 CONSTRAINT pk_estado PRIMARY KEY(id),
 CONSTRAINT coluna_ativo_deve_ser_S_ou_N CHECK (ativo IN('S', 'N')) -- CONSTRAINT OU OF LINE 'REGRAS DEFINIDAS FORA DA LINHA DE DEFINIÇÃO DA COLUNA'
 );
-/**
-INSERT INTO estado(id, nome, sigla, ativo, data_cadastro) VALUE('1', 'PARANÁ', 'PR', 'S', '2022-08-30');
-INSERT INTO estado(id, nome, sigla, ativo, data_cadastro) VALUE('2', 'GOIAS', 'GO', 'S', '2022-05-30');
-INSERT INTO estado(id, nome, sigla, ativo, data_cadastro) VALUE('3', 'MATO GROSSO', 'GO', 'S', '2022-04-30');
-*/
+
 INSERT INTO estado (nome, sigla) VALUES ('PARANÁ', 'PR');
 INSERT INTO estado (nome, sigla) VALUES ('GOIAS', 'GO');
 INSERT INTO estado (nome, sigla) VALUES ('MATO GROSSO', 'MT');
 
 SELECT id, nome, sigla, ativo, data_cadastro FROM estado;
+
+/** 
+VERIFICANDO A PADRONIZAÇÃO DO CÓDIGO
+*/
